d844258b4d19700688c873af3c23f8ceb784019d
sazzadmahmud85 <mahmudsazzad85@gmail.com>
2022-04-13 14:48:34 +0600

    customize the look and field of admin notice

--- admin-notice.php
+++ admin-notice.php
@@ -16,9 +16,10 @@ function nn_admin_notice(){
     if(!(isset($_COOKIE['nn-close']) && $_COOKIE['nn-close'] == 1)){
         if(in_array($pagenow, ['index.php', 'themes.php'])){
             ?>
-            <div id="noticeninja" class="notice notice-success is-dismissible">
+            <div id="noticeninja" class="notice notice-custom notice-success is-dismissible">
                 <h1>Here is a Heading</h1>
                 <p>Hey, here is some information for you.</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReAamAasjouviRtJXa-iCKPgLbjYnKaTbbfYuD_72ak4-x6kOM8U1u-O9MLj1OkL2uVSc&usqp=CAU" alt="">
             </div>
             <?php
         }
@@ -28,6 +29,7 @@ add_action('admin_notices', 'nn_admin_notice');
 
 add_action('admin_enqueue_scripts', function(){
     wp_enqueue_script('noticeninja-js', plugin_dir_url(__FILE__)."assets/js/script.js", array('jquery'), time());
+    wp_enqueue_style('noticeninja-css', plugin_dir_url(__FILE__)."assets/css/style.css", null, time());
 });
 
 
